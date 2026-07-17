3c4a9991762ee35e6fe683a0a4f145918e0c969a
the batman <aryansaharawat1001@gmail.com>
2026-07-17 16:00:18 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/response", async (req, res) => {
+
+    const { answer, date } = req.body;
+
+    const message =
+`🎉 She responded!
+
+Answer : ${answer}
+
+Date : ${date}
+
+Time : ${new Date().toLocaleString()}`;
+
+    try {
+
+        await axios.post(
+            `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+            {
+                chat_id: process.env.CHAT_ID,
+                text: message
+            }
+        );
+
+        res.json({
+            success:true
+        });
+
+    } catch (err) {
+    console.error("Telegram Error:");
+
+    if (err.response) {
+        console.log(err.response.data);
+    } else {
+        console.log(err.message);
+    }
+
+    res.status(500).json({
+        success: false
+    });
+}
+}
+
+    
+
+);
+
+app.listen(process.env.PORT,()=>{
+
+    console.log("Server Running");
+
+});
